0a1701fc48bd4675193d67979de4b5378e748556
itskrutinewalkar <newalkarkruti@gmail.com>
2024-10-12 01:52:46 +0530

    Modifications for Vercel

--- src/assets/index.js
+++ src/assets/index.js
@@ -73,17 +73,17 @@ import notification2 from "./notification/image-2.png";
 import notification3 from "./notification/image-3.png";
 import notification4 from "./notification/image-4.png";
 
-import benefitCard1 from "../../public/benefits/card-1.svg";
-import benefitCard2 from "../../public/benefits/card-2.svg";
-import benefitCard3 from "../../public/benefits/card-3.svg";
-import benefitCard4 from "../../public/benefits/card-4.svg";
-import benefitCard5 from "../../public/benefits/card-5.svg";
-import benefitCard6 from "../../public/benefits/card-6.svg";
-import benefitIcon1 from "../../public/benefits/icon-1.svg";
-import benefitIcon2 from "../../public/benefits/icon-2.svg";
-import benefitIcon3 from "../../public/benefits/icon-3.svg";
-import benefitIcon4 from "../../public/benefits/icon-4.svg";
-import benefitImage2 from "../../public/benefits/image-2.png";
+import benefitCard1 from "./benefits/card-1.png";
+import benefitCard2 from "./benefits/card-2.png";
+import benefitCard3 from "./benefits/card-3.png";
+import benefitCard4 from "./benefits/card-4.png";
+import benefitCard5 from "./benefits/card-5.png";
+import benefitCard6 from "./benefits/card-6.png";
+import benefitIcon1 from "./benefits/icon-1.svg";
+import benefitIcon2 from "./benefits/icon-2.svg";
+import benefitIcon3 from "./benefits/icon-3.svg";
+import benefitIcon4 from "./benefits/icon-4.svg";
+import benefitImage2 from "./benefits/image-2.png";
 
 import discordBlack from "./socials/discord.svg";
 import facebook from "./socials/facebook.svg";
